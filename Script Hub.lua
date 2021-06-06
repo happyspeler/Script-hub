86d678315beaa62a3827203f6ebb0b8e87c3b45b
happyspeler <mennom2007@gmail.com>
2021-06-06 13:52:11 +0200

    Create Script Hub.lua

---
+++ Script Hub.lua
@@ -0,0 +1,13 @@
+if game.PlaceId == 2041312716 then
+	loadstring(game:HttpGet(('https://gitlab.com/Tsuniox/lua-stuff/-/raw/master/RagdollEngineGUI.lua'),true))()
+	print("Game detected! Ragdoll Engine")
+else
+	print("Nope")
+end
+if game.PlaceId == 286090429 then
+	loadstring(game:HttpGet(('https://pastebin.com/raw/Li3UQD0b'),true))()
+	print("Game detected! Arsenal")
+else
+	print("Nope")
+end
+
