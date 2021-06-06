2636af23f8a2c4a2a9fe1676cf214c950a3bbb29
Menno <70851059+happyspeler@users.noreply.github.com>
2021-06-06 15:24:27 +0200

    Update Script Hub.lua

--- Script Hub.lua
+++ Script Hub.lua
@@ -1,13 +1,22 @@
+-- Configuration
 local Selexity = false -- Set to true if you want to load Selexity hub
+
+-- Welcome message
+local h = Instance.new("Hint")
+h.Parent = Workspace 
+h.Text = "Thanks for using my script hub."
+wait(5)
+h:Remove()
+
+if Selexity = true then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/happyspeler/Script-hub/main/Selexity.lua'),true))()
+end
 
 wait(4)
 
 -- Ragdoll engine
 if game.PlaceId == 2041312716 then
 	loadstring(game:HttpGet(('https://raw.githubusercontent.com/happyspeler/Script-hub/main/RagdollEngine.lua'),true))()
-	print("Game detected! Ragdoll Engine")
-else
-	print("Nope")
 end
 
 wait(2)
@@ -15,9 +24,6 @@ wait(2)
 -- Arsenal
 if game.PlaceId == 286090429 then
 	loadstring(game:HttpGet(('https://raw.githubusercontent.com/happyspeler/Script-hub/main/Arsenal.lua'),true))()
-	print("Game detected! Arsenal")
-else
-	print("Nope")
 end
 
 wait(2)
@@ -25,12 +31,4 @@ wait(2)
 -- Murder Mystery 2
 if game.PlaceId == 142823291 then
     loadstring(game:GetObjects("rbxassetid://6193945654")[1].Source)()
-end
-
-wait(2)
-
--- Selexity loader
-
-if Selexity = true then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/happyspeler/Script-hub/main/Selexity.lua'),true))()
 end
